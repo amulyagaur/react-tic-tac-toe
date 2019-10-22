fc8fb47174d0673bde58486ff9b199d7da5ff68b
amulya <amulyagaur111@gmail.com>
2019-10-22 08:46:28 +0530

    Implemented tied functionality.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button,Container,Row,Col, Alert,OverlayTrigger,Tooltip,Spinner,Jumbotron} from 'react-bootstrap';
+import {Button,Container,Row,Col, Alert,OverlayTrigger,Tooltip,Spinner,Jumbotron,ProgressBar} from 'react-bootstrap';
 
 
 function calculateWinner(squares) {
@@ -39,8 +39,8 @@ class Square extends React.Component {
         color: white;
         font-size: 45px;
         padding: 0;
-        height: 140px;
-        width: 140px;
+        height: 120px;
+        width: 120px;
         border: 1px solid #999;
         font-weight: bold;
         line-height: 34px;
@@ -96,71 +96,12 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      history:[{
-        squares: Array(9).fill(null)
-      }],
-      stepNumber:0,
-      xIsNext: true
-    }
-  }
-
-  changeValue = (i)=>{
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <>
-        <li key={move} >
-          <Button variant="info" onClick={() => this.jumpTo(move)}>{desc}</Button>
-        </li>
-        </>
-      );
-    });
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-      alert("🎉🎉🎉🎉 Congratulations... "+ status + " 🎉🎉🎉🎉");
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    return (
-      <div className="game" >
-        <Jumbotron fluid>
-          <Container>
-          <Spinner animation="grow" variant="primary" />
+class AnimatedCircles extends React.Component{
+  render()
+  {
+    return(
+      <div>
+      <Spinner animation="grow" variant="primary" />
           <Spinner animation="grow" variant="secondary" />
   <Spinner animation="grow" variant="success" />
   <Spinner animation="grow" variant="danger" />
@@ -194,54 +135,101 @@ class Game extends React.Component {
   <Spinner animation="grow" variant="danger" />
   <Spinner animation="grow" variant="warning" />
   <Spinner animation="grow" variant="info" /> 
+  </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      history:[{
+        squares: Array(9).fill(null)
+      }],
+      stepNumber:0,
+      xIsNext: true
+    }
+  }
+
+  changeValue = (i)=>{
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    const steps = this.state.stepNumber;
+    if (calculateWinner(squares) || squares[i] || steps===9) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let steps = this.state.stepNumber;
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <>
+        <li key={move} >
+          <Button variant="info" onClick={() => this.jumpTo(move)}>{desc}</Button>
+        </li>
+        </>
+      );
+    });
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+      alert("🎉🎉🎉🎉 Congratulations... "+ status + " 🎉🎉🎉🎉");
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    if(steps===9 && !winner)
+    {
+      status='Match Tied!';
+      alert('Match Tied!');
+    }
+      
+    return (
+      <div className="game" >
+        <Jumbotron fluid>
+          <Container>
+          <AnimatedCircles/>
           <center><h1>Tic Tac Toe - Awesome Childhood Game</h1></center>
-          <Spinner animation="grow" variant="primary" />
-  <Spinner animation="grow" variant="secondary" />
-  <Spinner animation="grow" variant="success" />
-  <Spinner animation="grow" variant="danger" />
-  <Spinner animation="grow" variant="warning" />
-  <Spinner animation="grow" variant="info" />
-  <Spinner animation="grow" variant="light" />
-  <Spinner animation="grow" variant="dark" />
-  <Spinner animation="grow" variant="secondary" />
-  <Spinner animation="grow" variant="success" />
-  <Spinner animation="grow" variant="danger" />
-  <Spinner animation="grow" variant="warning" />
-  <Spinner animation="grow" variant="info" />
-  <Spinner animation="grow" variant="light" />
-  <Spinner animation="grow" variant="dark" />
-  <Spinner animation="grow" variant="secondary" />
-  <Spinner animation="grow" variant="success" />
-  <Spinner animation="grow" variant="danger" />
-  <Spinner animation="grow" variant="warning" />
-  <Spinner animation="grow" variant="info" />
-  <Spinner animation="grow" variant="light" />
-  <Spinner animation="grow" variant="dark" />
-  <Spinner animation="grow" variant="secondary" />
-  <Spinner animation="grow" variant="success" />
-  <Spinner animation="grow" variant="danger" />
-  <Spinner animation="grow" variant="warning" />
-  <Spinner animation="grow" variant="info" />
-  <Spinner animation="grow" variant="light" />
-  <Spinner animation="grow" variant="dark" />
-  <Spinner animation="grow" variant="secondary" />
-  <Spinner animation="grow" variant="success" />
-  <Spinner animation="grow" variant="danger" />
-  <Spinner animation="grow" variant="warning" />
-  <Spinner animation="grow" variant="info" /> 
+          <AnimatedCircles/>
           </Container>
         </Jumbotron>
+        <ProgressBar animated now={(100*steps)/9} variant="warning" />
+        <br></br>
         <Container>
   <Row>
     <Col>
-        <div className="game-board">
+        <div className="game-board" >
           <Board squares={current.squares} handleClick={(i)=>this.changeValue(i)}/>
         </div>
     </Col>
     <Col>
         <div className="game-info">
           <div>
-            <Alert variant={winner ? "success" : "primary"}>
+            <Alert variant={winner ? "success" : (steps===9 ?"danger":"primary")}>
           <h2>{ status }</h2>
           </Alert>
           </div>
@@ -251,6 +239,8 @@ class Game extends React.Component {
       
       </Row>
       </Container>
+      <br></br>
+      
       </div>
     );
   }
